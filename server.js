1d17b1b14f604ffb9d24393c98663000ef83a95c
Javier Delgado <javierdelgado@Javiers-MacBook-Pro.local>
2022-04-09 15:26:37 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+const pokemon = require('./models/pokemon');
+const methodOverride = require('method-override');
+const morgan = require('morgan');
+
+////////////////////////
+// Setup - Import deps and create app object
+////////////////////////
+//////////////////////
+// Declare Middleware
+//////////////////////
+app.use(methodOverride('_method'))
+app.use(express.urlencoded({extended: false}));
+app.use(morgan('tiny'));
+app.use('/public', express.static('public'));
+
+///////////////////////
+// Declare Routes and Routers 
+///////////////////////
+app.get('/', (req, res) => {
+    res.send('Root');
+})
+
+
+
+///////////////////////////
+// Server Listener
+///////////////////////////
+app.listen(PORT, (req, res) => {
+    console.log(`Server up on port: ${PORT}`)
+});
